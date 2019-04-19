873c1aece855b015afdeb64e08b65bf8e1703965
pmartin36 <pmartin36@gmail.com>
2019-04-18 23:15:23 -0400

    extract cell basic

--- Assets/Scripts/HexInfo.cs
+++ Assets/Scripts/HexInfo.cs
@@ -2,33 +2,14 @@
 using UnityEngine;
 
 [Serializable]
-public class HexInfo {
-	public bool Occupied { 
+public class HexInfo : HexInfoBasic {
+	public override bool Occupied { 
 		get => Filled || TowerHead != null;
 	}
-
-	public bool Filled { get; private set; }
-	public bool Locked { get; private set; } // whatever this point is assigned to originally cannot be replaced
-
 	public Tower TowerHead { get; set; }
 
-	public Vector3 PhysicalCoordinates;
-	public HexCoordinates Coordinates;
-
-	public int NumTouchedWalls { get; set; }
-	public bool Reachable { get; set; }
-
-	public HexCell Cell;
-
-	public HexInfo(int x, int y, HexMetrics metrics): this(x, y, metrics, false, false, false) { }
-	public HexInfo(int x, int y, HexMetrics metrics, bool fill, bool locked, bool reachable) {
-		PhysicalCoordinates = metrics.RepresentationalCoordinatesToWorldCoordinates(x, y);
-
-		this.Coordinates = HexCoordinates.FromRepresentationalCoordinates(x, y);
-		this.Filled = fill;
-		this.Locked = locked;
-		this.Reachable = reachable;
-		this.NumTouchedWalls = -1;
-	}
+    public HexInfo(int x, int y, HexMetrics metrics) : base(x, y, metrics, false, false, false) { }
+    public HexInfo(int x, int y, HexMetrics metrics, bool fill, bool locked, bool reachable)
+        : base(x, y, metrics, fill, locked, reachable){ }
 }
 
---
+++ Assets/Scripts/HexInfoBasic.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using UnityEngine;
+
+[Serializable]
+public class HexInfoBasic {
+	public virtual bool Occupied { 
+		get => Filled;
+	}
+
+	public bool Filled { get; protected set; }
+	public bool Locked { get; protected set; } // whatever this point is assigned to originally cannot be replaced
+
+	public Vector3 PhysicalCoordinates;
+	public HexCoordinates Coordinates;
+
+	public int NumTouchedWalls { get; set; }
+	public bool Reachable { get; set; }
+
+	public HexCell Cell;
+
+	public HexInfoBasic(int x, int y, HexMetrics metrics): this(x, y, metrics, false, false, false) { }
+	public HexInfoBasic(int x, int y, HexMetrics metrics, bool fill, bool locked, bool reachable) {
+		PhysicalCoordinates = metrics.RepresentationalCoordinatesToWorldCoordinates(x, y);
+
+		this.Coordinates = HexCoordinates.FromRepresentationalCoordinates(x, y);
+		this.Filled = fill;
+		this.Locked = locked;
+		this.Reachable = reachable;
+		this.NumTouchedWalls = -1;
+	}
+}
+
